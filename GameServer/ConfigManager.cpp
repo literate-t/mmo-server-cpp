36fde9fa09dee62bad7027a0cc012311f6bc10dc
literate-t <gaekoon@gmail.com>
2025-03-17 23:51:00 +0900

    feat: json 데이터에서 dataPath 항목을 획득한다

--- GameServer/ConfigManager.cpp
+++ GameServer/ConfigManager.cpp
@@ -1,16 +1,15 @@
 #include "pch.h"
 #include "ConfigManager.h"
 #include "FileManager.h"
-#include <fstream>
-#include <sstream>
+#include "nlohmann/json.hpp"
 
 ConfigManager::ConfigManager()
 {
 	LoadConfig();
-	}
+}
 
 void ConfigManager::LoadConfig()
-	{
+{
 	string path = "D:/SourceCode/GameSource/MMO_Basic_Cpp/Common/Config/config.json";
 
 	string file_content = FileManager::GetContent(path);
@@ -19,28 +18,10 @@ void ConfigManager::LoadConfig()
 		Deserialize(file_content, "dataPath");
 }
 
-string ConfigManager::Deserialize(string key)
+void ConfigManager::Deserialize(string json_content, string key)
 {
-	//// TODO
-	// example
-	//using json = nlohmann::json;
+	using json = nlohmann::json;
 
-	//struct Person {
-	//	std::string name;
-	//	int age;
-	//};
-
-	//// 사용자 정의 타입 Person에 대한 from_json 함수 정의
-	//void from_json(const json & j, Person & p) {
-	//	j.at("name").get_to(p.name);
-	//	j.at("age").get_to(p.age);
-	//}
-	
-	// std::vector<Person> persons = j.at("persons").get<std::vector<Person>>();
-	// Person person = j.get<Person>();
-}
-
-string ConfigManager::GetDataPath()
-{
-	return _data_path;
+	json j = json::parse(json_content);
+	j.at(key).get_to(_data_path);
 }
