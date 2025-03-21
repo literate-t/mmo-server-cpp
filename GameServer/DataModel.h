ecfc1bf494a779b46fc4a60df49da9b14ffffcde
literate-t <gaekoon@gmail.com>
2025-03-21 22:37:25 +0900

    add: items 테이블과 동일한 구조의 구조체를 정의한다

---
+++ GameServer/DataModel.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "pch.h"
+
+namespace DataModel
+{
+	struct ItemDB
+	{
+		int32 ItemDbId;
+		int32 DataSheetId;
+		int32 Count;
+		int32 Slot;
+		bool Equipped;
+		int32 OwnerId;
+	};
+}
