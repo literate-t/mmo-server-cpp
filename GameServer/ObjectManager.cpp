e1b10fb1f49fdc758194c227750b9de2730dd1ed
literate-t <gaekoon@gmail.com>
2025-03-21 22:51:05 +0900

    feat: Object를 관리할 ObjectManager 클래스를 정의한다
    - 오브젝트를 생성할 때 오브젝트의 타입과 자체 카운터를 통해 고유 오브젝트 아이디를 생성한다

---
+++ GameServer/ObjectManager.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "ObjectManager.h"
+
+ObjectManager ObjectManager::Instance;
+
+int32 ObjectManager::GenerateId(GameObjectType type)
+{
+	WRITE_LOCK;
+	return (int32)type << 24 | _counter++;
+}
+
+GameObjectType ObjectManager::GetObjectTypeById(int32 object_id)
+{
+	// 0x7F로 마스킹해 결과를 보장한다
+	return GameObjectType(object_id >> 24 & 0x7F);
+}
---
+++ GameServer/ObjectManager.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Protocol.pb.h"
+using namespace Protocol;
+#include "GameObject.h"
+
+class ObjectManager
+{
+public:
+	ObjectManager(const ObjectManager&) = delete;
+	ObjectManager& operator=(const ObjectManager&) = delete;
+
+	static ObjectManager Instance;
+
+	template<typename T>
+	shared_ptr<T> Add()
+	{
+		static_assert(is_base_of_v<GameObject, T>, "T type must be derived from GameObject");
+		static_assert(is_default_constructible_v<T>, "T type must be default constructible");
+
+		shared_ptr<T> object = make_shared<T>();
+
+		{
+			WRITE_LOCK;
+			object->Id(GenerateId(object->GetType()));
+		}
+		
+		return object;
+	}
+
+	int32 GenerateId(GameObjectType type);
+	static GameObjectType GetObjectTypeById(int32 object_id);
+
+private:
+	ObjectManager() = default;
+
+	// [UNUSED(1)|TYPE(7)][      ID(24)      ]
+	int32 _counter = 1;
+
+	USE_LOCK;
+};
+
