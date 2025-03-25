c6ee185d920a499c6b17666b38bd7155e56630fb
literate-t <gaekoon@gmail.com>
2025-03-25 22:47:36 +0900

    feat: shared_ptr<Inventory>를 SharedInventory 별칭으로 사용한다

--- GameServer/Types.h
+++ GameServer/Types.h
@@ -4,3 +4,4 @@ using SharedGameSession = shared_ptr<class GameSession>;
 using SharedPlayer = shared_ptr<class Player>;
 using SharedRoom = shared_ptr<class Room>;
 using SharedItem = shared_ptr<class Item>;
+using SharedInventory = shared_ptr<class Inventory>;
