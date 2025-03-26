6f0b926405ba7578a37afd32ecc1bc6f5300f497
literate-t <gaekoon@gmail.com>
2025-03-26 20:41:17 +0900

    feat: Map 클래스를 추가한다
    - 세부 API 작업은 추후에 진행한다

---
+++ GameServer/Map.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "Map.h"
+#include "GameObject.h"
+
+bool Map::ApplyMove(SharedObject object, Vector2Int dest, bool is_through_objects, bool apply_collision)
+{
+	return true;
+}
---
+++ GameServer/Map.h
@@ -0,0 +1,9 @@
+#pragma once
+class Map
+{
+public:
+	bool ApplyMove(SharedObject object, Vector2Int dest, bool is_through_objects = true, bool apply_collision = true);
+
+	// TODO
+};
+
