f721e1934acc4cecb6254e9c68d1fd67152ae2ba
literate-t <gaekoon@gmail.com>
2025-03-26 20:39:04 +0900

    feat: 방의 Player 입장을 처리한다
    - 오브젝트 타입을 통해 Player를 식별한다
    - 맵에 플레이어의 위치를 지정한다
    - 플레이어의 좌표를 통해 Zone을 구하고 해당 Zone에 Player를 추가한다
    - View 객체를 갱신한다

--- GameServer/Room.cpp
+++ GameServer/Room.cpp
@@ -2,11 +2,42 @@
 #include "Room.h"
 #include "Player.h"
 #include "GameSession.h"
+#include "ObjectManager.h"
+#include "Map.h"
+#include "Zone.h"
+#include "ClientPacketHandler.h"
+#include "ViewCube.h"
 
 void Room::Enter(SharedPlayer player)
 {
-	//WRITE_LOCK;
+	if (object == nullptr)
+		return;
+
+	// TODO: random position
+
+	GameObjectType type = g_object_manager->GetObjectTypeById(object->GetType());
+
+	if (type == GameObjectType::PLAYER)
+	{
+		SharedPlayer player = static_pointer_cast<Player>(object);
 	_players[player->PlayerDbId] = player;
+		
+		player->SetRoom(static_pointer_cast<Room>(shared_from_this()));
+		player->RefreshStat();
+
+		g_map->ApplyMove(player, Vector2Int(player->GetPositionInfo().posx(), player->GetPositionInfo().posy()));
+		GetZone(player->GetCellPos()).GetPlayers().insert(player);
+
+		// 접속 본인 정보 전송
+		{
+			S_EnterGame enter_game;
+			enter_game.set_allocated_player(&player->GetObjectInfo());
+			player->OwnerSession->Send(ClientPacketHandler::MakeSendBuffer(enter_game));
+		}
+
+		player->GetView()->IsReset = true;
+		player->GetView()->Update();
+	}
 }
 
 void Room::Leave(SharedPlayer player)
