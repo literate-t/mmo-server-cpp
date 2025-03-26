1e85d8a25d8900ac6da24d1124db7e2458a7112d
literate-t <gaekoon@gmail.com>
2025-03-26 20:52:19 +0900

    feat: ViewCube의 Getter를 정의한다

--- GameServer/Player.h
+++ GameServer/Player.h
@@ -11,11 +11,14 @@ public:
 	uint64 PlayerDbId = 0;
 	string name;
 	// caution: cycle
-	SharedGameSession OwnerSession;
+	SharedGameSession OwnerSession;	
 
 	Inventory& GetInventory() { return _inventory; }
 	void SetRoom(SharedRoom room) { _room = room; }
 	void RefreshStat();	
+
+	SharedViewCube& GetView() { return _view_cube; }
+
 private:
 	Inventory _inventory;
 	weak_ptr<class Room> _room;
