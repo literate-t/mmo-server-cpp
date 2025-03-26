734b9bf98c6014f508384a2490a907f4367b8c51
literate-t <gaekoon@gmail.com>
2025-03-26 20:52:54 +0900

    feat: SharedViewCube 별칭으로 shared_ptr<ViewCube>를 사용한다

--- GameServer/Types.h
+++ GameServer/Types.h
@@ -5,3 +5,4 @@ using SharedPlayer = shared_ptr<class Player>;
 using SharedRoom = shared_ptr<class Room>;
 using SharedItem = shared_ptr<class Item>;
 using SharedInventory = shared_ptr<class Inventory>;
+using SharedViewCube = shared_ptr<class ViewCube>;
