5dbb9c8a0390d1090ddc7d57064fbdf2f442fb21
literate-t <gaekoon@gmail.com>
2025-03-26 20:53:22 +0900

    feat: 클라이언트와 통신하기 위해 x, y를 가지는 Vector2Int 구조체를 정의한다

--- GameServer/Types.h
+++ GameServer/Types.h
@@ -6,3 +6,31 @@ using SharedRoom = shared_ptr<class Room>;
 using SharedItem = shared_ptr<class Item>;
 using SharedInventory = shared_ptr<class Inventory>;
 using SharedViewCube = shared_ptr<class ViewCube>;
+
+struct Vector2Int
+{
+	int x;
+	int y;
+
+	Vector2Int(int x, int y) : x(x), y(y) {}
+
+	static Vector2Int Up() { return Vector2Int(0, 1); }
+	static Vector2Int Down() { return Vector2Int(0, -1); }
+	static Vector2Int Left() { return Vector2Int(-1, 0); }
+	static Vector2Int Right() { return Vector2Int(1, 0); }
+
+	Vector2Int operator+(const Vector2Int& rhs)
+	{
+		return Vector2Int(x + rhs.x, y + rhs.y);
+	}
+
+	Vector2Int operator-(const Vector2Int& rhs)
+	{
+		return Vector2Int(x - rhs.x, y - rhs.y);
+	}
+
+	const int32 SquareMagnitude = x * x + y * y;
+	float Magnitude() { return static_cast<float>(sqrt(SquareMagnitude)); }
+
+	const int32 SimpleDistance = abs(x) + abs(y);
+};
