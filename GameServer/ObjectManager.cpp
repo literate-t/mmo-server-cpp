8b89a3ba0e431897c26bd845098c3a6bab7e7e9f
literate-t <gaekoon@gmail.com>
2025-03-27 11:44:54 +0900

    fix: 연산자 우선순위를 고려해 시프트 연산에 괄호를 추가한다

--- GameServer/ObjectManager.cpp
+++ GameServer/ObjectManager.cpp
@@ -10,5 +10,5 @@ int32 ObjectManager::GenerateId(GameObjectType type)
 GameObjectType ObjectManager::GetObjectTypeById(int32 object_id)
 {
 	// 0x7F로 마스킹해 결과를 보장한다
-	return GameObjectType(object_id >> 24 & 0x7F);
+	return GameObjectType((object_id >> 24) & 0x7F);
 }
