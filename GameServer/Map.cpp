1bf6cd97e386f7df1ba491e84054097d382496c3
literate-t <gaekoon@gmail.com>
2025-03-27 20:43:30 +0900

    feat: 2차원 배열을 초기화한다
    - _collisions, _objects를 초기화한다
    - row가 x축, col이 y축을 나타내도록 한다

--- GameServer/Map.cpp
+++ GameServer/Map.cpp
@@ -1,6 +1,26 @@
 #include "pch.h"
 #include "Map.h"
 #include "GameObject.h"
+void Map::InitArrays(int32 row, int32 col)
+{
+	// _collisions
+	_collisions.resize(col);
+	for (int i = 0; i < _collisions.size(); ++i)
+	{
+		_collisions[i].resize(row);
+		for (int j = 0; j < _collisions[i].size(); ++j)
+			_collisions[i][j] = false;
+	}
+
+	// _objects
+	_objects.resize(col);
+	for (int i = 0; i < _objects.size(); ++i)
+	{
+		_objects[i].resize(row);
+		for (int j = 0; j < _objects[i].size(); ++j)
+			_objects[i][j] = nullptr;
+	}
+}
 
 bool Map::ApplyMove(SharedObject object, Vector2Int dest, bool is_through_objects, bool apply_collision)
 {
