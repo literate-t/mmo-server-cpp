554a891c7c313f0bb38e251f39f99d2cc33b2bb8
literate-t <gaekoon@gmail.com>
2025-03-27 20:42:31 +0900

    feat: Map.h를 작성한다
    - 각 함수는 소스 코드에서 간단하게 설명한다
    - _collisions, _objects를 통해 충돌과 오브젝트를 관리한다

--- GameServer/Map.h
+++ GameServer/Map.h
@@ -2,8 +2,34 @@
 class Map
 {
 public:
-	bool ApplyMove(SharedObject object, Vector2Int dest, bool is_through_objects = true, bool apply_collision = true);
+	void LoadMap(int32 map_id, string prefix_path = "D:/SourceCode/GameSource/MMO_Basic_Cpp/Common/MapData");
+	bool ApplyMove(SharedObject object, Vector2Int dest, bool is_through_objects = false, bool apply_collision = true);
+	bool CanGo(Vector2Int cell_pos, bool, bool is_through_objects = false);
 
-	// TODO
+	Vector2Int GetCoordIndex(Vector2Int cell_pos);
+	Vector2Int GetCoordIndex(int x, int y);
+
+	int32 GetMinX() const { return _minx; }
+	int32 GetMinY() const { return _miny; }
+	int32 GetMaxX() const { return _maxx; }
+	int32 GetMaxY() const { return _maxy; }
+
+	int32 GetSizeX() const { return _maxx - _minx + 1; }
+	int32 GetSizeY() const { return _maxy - _miny + 1; }
+
+private:
+	void InitArrays(int32 row, int32 col);
+	bool HasCollision(Vector2Int index_pos, bool is_through_objects);
+	bool IsWithinBounds(int32 x, int32 y);
+	void SetCurrentZone(SharedObject object, Vector2Int dest);
+
+private:
+	int32 _minx;
+	int32 _miny;
+	int32 _maxx;
+	int32 _maxy;
+
+	xvector<xvector<bool>> _collisions;
+	xvector<xvector<SharedObject>> _objects;
 };
 
