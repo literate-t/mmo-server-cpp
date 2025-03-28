069a2c0dc8b1a80ecee23ce9b0411392bfaa193d
literate-t <gaekoon@gmail.com>
2025-03-28 16:21:22 +0900

    feat: Zone에 있는 오브젝트를 제거하는 Remove() 함수를 정의한다

--- GameServer/Zone.cpp
+++ GameServer/Zone.cpp
@@ -1,7 +1,21 @@
 #include "pch.h"
 #include "Zone.h"
+#include "Protocol.pb.h"
+using namespace Protocol;
+#include "ObjectManager.h"
 
 unordered_set<SharedPlayer>& Zone::GetPlayers()
 {
     return _players;
+}
+
+void Zone::Remove(SharedObject object)
+{
+    GameObjectType type = g_object_manager->GetObjectTypeById(object->GetObjectId());
+
+    switch (type)
+    {
+    case GameObjectType::PLAYER:
+        _players.erase(SharedCast<Player>(object));
+    }
 }
--- GameServer/Zone.h
+++ GameServer/Zone.h
@@ -3,6 +3,7 @@ class Zone
 {
 public:
 	unordered_set<SharedPlayer>& GetPlayers();
+	void Remove(SharedObject object);
 
 private:
 	unordered_set<SharedPlayer> _players;
