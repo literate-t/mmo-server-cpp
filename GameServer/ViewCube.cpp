fbbd5ccb760728f683c1be75a589c3371e9798d1
literate-t <gaekoon@gmail.com>
2025-03-31 17:14:33 +0900

    feat: 새로 스폰되거나 디스폰된 오브젝트를 식별해 해당 정보를 클라이언트로 보낸다
    - 이 작업을 100ms마다 반복하기 위해 잡큐에 넣는다

--- GameServer/ViewCube.cpp
+++ GameServer/ViewCube.cpp
@@ -8,9 +8,45 @@ ViewCube::ViewCube(SharedPlayer player)
 
 void ViewCube::Update()
 {
+	SharedRoom room = _owner->GetRoom();
+	if (_owner == nullptr || room == nullptr)
+	{
+		_objects.clear();
+		return;
+	}
+
+	const xhash_set<SharedObject>& current_objects = GetObjects();
+
+	// Spawn object
+	const auto& added = Except(current_objects, _prev_objects);
+	if (added.size() > 0)
+	{
+		S_Spawn spawn_packet;
+		for (const auto& item : added)
+		{
+			ObjectInfo* object_info = spawn_packet.add_objects();
+			object_info->MergeFrom(item->GetObjectInfo());
+		}
+		_owner->OwnerSession->Send(ClientPacketHandler::MakeSendBuffer(spawn_packet));
+	}
+
+	// Despawn object
+	const auto& vanished = Except(_prev_objects, current_objects);
+	if (vanished.size() > 0)
+	{
+		S_Despawn despawn_packet;
+		for (const auto& item : vanished)
+			despawn_packet.add_objectids(item->GetObjectId());
+
+		_owner->OwnerSession->Send(ClientPacketHandler::MakeSendBuffer(despawn_packet));
 }
 
-const unordered_set<SharedObject>& ViewCube::GetObjects()
+	_prev_objects.clear();
+	for (const auto& item : current_objects)
+		_prev_objects.insert(item);
+
+	room->PushTimerAsync(100, [this](){ Update();});
+}
 {
 
 	return _objects;
