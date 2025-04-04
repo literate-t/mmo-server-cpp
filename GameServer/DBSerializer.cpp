5c83ed0c7de342541c29202c7541ea4fdcea6aad
literate-t <gaekoon@gmail.com>
2025-04-04 16:49:04 +0900

    feat: 스탯을 DB에 저장한다
    - 지금은 hp만 구현한다

--- GameServer/DBSerializer.cpp
+++ GameServer/DBSerializer.cpp
@@ -5,11 +5,23 @@
 
 void DBSerializer::SavePlayerStatusAll(SharedPlayer player)
 {
-	if (player == nullptr || room == nullptr)
+	if (player == nullptr)
 		return;
 
-	// TODO : db¿¡ ÀúÀå
-	Instance().PushJobAsync([&player, &room](){
+	// Save hp to db for now
+	Instance()->PushJobAsync([player]() {
+		DBConnection* conn = g_db_connection_pool->Pop();
+		auto query = L"UPDATE stats SET hp = (?) WHERE player_id = (?)";
 
+		DBBind<2, 0> db_bind(*conn, query);
+		int32 hp = player->GetStatInfo().hp();
+
+		db_bind.BindParam(0, hp);
+		db_bind.BindParam(1, player->PlayerDbId);
+
+		if (!db_bind.Execute())
+			throw new runtime_error("DB Failed: SavePlayerStatusAll()");
+
+		g_db_connection_pool->Push(conn);
 	});
 }
