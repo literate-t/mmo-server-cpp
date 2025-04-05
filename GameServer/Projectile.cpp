0d286e2737afb9a1ed1bbfcf56a9dfad1d339986
literate-t <gaekoon@gmail.com>
2025-04-05 22:22:55 +0900

    feat: 투사체를 관리할 Proejctile 클래스를 추가한다

--- GameServer/Projectile.cpp
+++ GameServer/Projectile.cpp
@@ -1,2 +1,7 @@
 #include "pch.h"
 #include "Projectile.h"
+
+Projectile::Projectile()
+	:GameObject(GameObjectType::PROJECTILE), Data(nullptr)
+{
+}
--- GameServer/Projectile.h
+++ GameServer/Projectile.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "GameObject.h"
+#include "Info.h"
 
 class Projectile : public GameObject
 {
+public:
+	Projectile();
+
+public:
+	SkillData* Data;
 };
 
