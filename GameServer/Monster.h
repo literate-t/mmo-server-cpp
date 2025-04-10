475449be7c165f5e6f44b1d8b6f30ab2d51a064e
literate-t <gaekoon@gmail.com>
2025-04-10 13:57:34 +0900

    feat: Monster 클래스를 선언한다

--- GameServer/Monster.h
+++ GameServer/Monster.h
@@ -1,7 +1,42 @@
 #pragma once
 #include "GameObject.h"
+#include "Info.h"
 
 class Monster : public GameObject
 {
-};
+public:
+	Monster();
+	~Monster();
 
+	void Init(int32 data_sheet_id);
+	void Update() override;
+
+protected:
+	virtual void UpdateIdle();
+	virtual void UpdateMoving();
+	virtual void UpdateSkill();
+	virtual void UpdateDead();
+	
+private:
+	void BroadcastState();
+	void StopTargeting();
+	RewardData GetRandomReward();
+
+public:
+	SharedJob JobReserved;
+
+private:
+	int32 _data_sheet_id;
+	StatData _stat_data;
+	SkillData _skill_data;
+	int16 _search_range = 10;
+	int16 _chase_range = 20;
+	int16 _skill_range = 1;
+
+	uint64 _skill_tick = 0;
+	uint64 _search_tick = 0;
+	uint64 _move_tick = 0;
+
+	SharedPlayer _target = nullptr;
+	SharedRoom _room;
+};
