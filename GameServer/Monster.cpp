8cc7260bc793e81df96c4658d9d5d62493883f6c
literate-t <gaekoon@gmail.com>
2025-04-10 14:17:21 +0900

    feat: Monster::Update()를 정의한다
    - EntityState 상태에 따라 호출되는 함수가 달라진다
    - Update() 함수는 200ms마다 자동으로 호출된다

--- GameServer/Monster.cpp
+++ GameServer/Monster.cpp
@@ -23,3 +23,30 @@ void Monster::Init(int32 data_sheet_id)
 	SetStatInfoWithStatData(data.stat);
 	SetState(EntityState::IDLE);
 }
+
+// FSM
+void Monster::Update()
+{
+	_room = GetRoom();
+	if (nullptr == _room)
+		return;
+
+	switch (GetState())
+	{
+	case EntityState::IDLE:
+		UpdateIdle();
+		break;
+	case EntityState::MOVING:
+		UpdateMoving();
+		break;
+	case EntityState::SKILL:
+		UpdateSkill();
+		break;
+	case EntityState::DEAD:
+		UpdateDead();
+		break;
+	}
+
+	auto shared_this = shared_from_this();
+	JobReserved = _room->PushTimerAsync(200, [shared_this]() { shared_this->Update(); });
+}
