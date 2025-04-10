fe34622108ed2cfc14b5974f88bd233ace7bef60
literate-t <gaekoon@gmail.com>
2025-04-10 14:00:15 +0900

    feat: 생성자와 소멸자를 정의한다
    - 생성자에서는 오브젝트 타입을 지정한다
    - 소멸자는 메모리 해제 확인이 필요할 때 로깅 목적으로 정의한다

--- GameServer/Monster.cpp
+++ GameServer/Monster.cpp
@@ -1,2 +1,17 @@
 #include "pch.h"
 #include "Monster.h"
+#include "DataManager.h"
+#include "Room.h"
+#include "Map.h"
+#include "Player.h"
+#include "ClientPacketHandler.h"
+
+Monster::Monster()
+	:GameObject(GameObjectType::MONSTER)
+{
+}
+
+Monster::~Monster()
+{
+	cout << "~Monster()" << endl;
+}
