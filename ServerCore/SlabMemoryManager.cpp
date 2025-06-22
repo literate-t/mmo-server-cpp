b16e250d277e75dcd2b68f2110ba7ea57630a860
literate-t <gaekoon@gmail.com>
2025-06-22 10:44:53 +0900

    feat: CentralPool + ThreadLocal 구조를 통해 캐시라인을 활용할 수 있는 구조를 적용한다.
    - 개선사항이 존재: CentralPool을 기존 PoolManager를 활용하지 않고 완전히 새롭게 새로 작성해서 이 구조에 맞는 최적화를 진행해야 할 것 같다.
    
    - 로컬 스레드에서 메모리를 Fetch()할 때 요청 카운트 만큼 반복문을 돌리는 로직을 제거하고 한 번에 청크를 가져와 쪼개서 다루는 방법을 고민해본다.

--- ServerCore/SlabMemoryManager.cpp
+++ ServerCore/SlabMemoryManager.cpp
@@ -1,10 +1,70 @@
 #include "pch.h"
+#include "SlabMemoryManager.h"
 
-void* CentralPool::allocate(int32 size)
+// ------- CentralPool ------- //
+void* CentralPool::Allocate(int32 size)
 {
-    return nullptr;
+    return PoolManager::Instance().Allocate(size);
 }
 
-void CentralPool::deallocate(void* ptr)
+void CentralPool::Release(void* ptr)
 {
+    PoolManager::Instance().Release(ptr);
+}
+
+// ------- ThreadLocalSlab ------- //
+void* ThreadLocalSlab::Allocate(int32 size)
+{
+    int32 index = SizeConverter::Instance().ToIndex(size);
+    auto& stack = _memory_stacks[index];
+
+    // 스택이 비어 있으면 중앙 풀에서 새로 받아온다
+    if (stack.Empty())
+        Fetch(index);
+
+    return stack.Pop();
+}
+
+void ThreadLocalSlab::Release(void* ptr)
+{
+    MemoryHeader* header = MemoryHeader::DetachHeader(ptr);
+    int32 index = SizeConverter::Instance().ToIndex(header->_alloc_size);
+    auto& stack = _memory_stacks[index];
+
+    if (stack.Full())
+        Flush(index);
+
+    stack.Push(ptr);
+}
+
+void ThreadLocalSlab::Fetch(int32 index)
+{
+    auto& stack = _memory_stacks[index];
+    int32 size = SizeConverter::Instance().GetArray()[index];
+
+    for (int i = 0; i < kFetchCount - stack.top; ++i)
+        stack.Push(CentralPool::Allocate(size));
+}
+
+void ThreadLocalSlab::Flush(int32 index)
+{
+    int32 size = SizeConverter::Instance().GetArray()[index];
+    auto& stack = _memory_stacks[index];
+
+    int32 to = stack.top / 2;
+    for (int32 i = 0; i < to; ++i)
+        CentralPool::Release(stack.Pop());
+}
+
+// ------- SizeConverter ------- //
+SizeConverter::SizeConverter()    
+{
+    generate(arr_size.begin(), arr_size.end(), []() { static int32 seed; return seed += 32; });
+    _size_count = static_cast<int32>(arr_size.size());
+}
+
+int32 SizeConverter::ToIndex(int32 size)
+{
+    auto find_iter = lower_bound(arr_size.begin(), arr_size.end(), size);
+    return static_cast<int32>(distance(arr_size.begin(), find_iter));
 }
--- ServerCore/SlabMemoryManager.h
+++ ServerCore/SlabMemoryManager.h
@@ -1,15 +1,70 @@
 #pragma once
+#include <iterator>
+
+const int kFetchCount= 32;
+const int kDataCount = 32;
+const int kArraySizeCount = 512 / 32;
 
 // ------- CentralPool ------- //
 class CentralPool
 {
 public:
-	void* allocate(int32 size);
-	void deallocate(void* ptr);
+	static void* Allocate(int32 size);
+	static void Release(void* ptr);
+};
+
+// ------- MemoryStack ------- //ss
+/*
+* { 32, 48, 64, 80, 96, 112, 128 ..} → index 0, 1, 2, 3, 4, 5, 6.. 
+* 각 사이즈 별로 고정된 크기의 메모리를 들고 있는다
+* 메모리를 관리할 별도의 구조체를 둔다
+*/
+template<int32 N>
+struct alignas(64) MemoryStack
+{
+	inline int32 Top() { return top; }
+
+	void* Pop() { return bufs[--top]; }
+	void Push(void* ptr) { bufs[top++] = ptr; }
+	bool Empty() { return top == 0; }
+	bool Full() { return top == N; }
+
+	void* bufs[N];
+	int32 top = 0;
+};
+// ------- SizeConverter ------- //
+class SizeConverter
+{
+public:
+	SizeConverter();
+
+	inline static SizeConverter& Instance()
+	{
+		static SizeConverter instance;
+		return instance;
+	}
+
+	int32 ToIndex(int32 size);
+
+	constexpr auto& GetArray() const { return arr_size; }
+
+private:
+	array<int32, kArraySizeCount> arr_size;
+	int32 _size_count;
 };
 
 // ------- ThreadLocalSlab ------- //
-class ThreadLocalSlab
-{
 
+class alignas(64) ThreadLocalSlab
+{
+public:
+	void* Allocate(int32 size);
+	void Release(void* ptr);
+
+private:
+	void Fetch(int32 index);
+	void Flush(int32 index);
+
+private:
+	array<MemoryStack<kDataCount>, kArraySizeCount> _memory_stacks;
 };
