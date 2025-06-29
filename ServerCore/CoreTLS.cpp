afe88fa8bbce58df85a22332b3eda06b3e161aef
literate-t <gaekoon@gmail.com>
2025-06-29 11:50:18 +0900

    feat: ThreadLocalSlab를 스레드 로컬에서 배열로 사용한다
    - 각 요소가 블록 사이즈를 담당하게 될 것이기 때문에 SizeConverter 클래스를 통해 블록 사이즈를 순차적으로 생성자에 넘긴다.

--- ServerCore/CoreTLS.cpp
+++ ServerCore/CoreTLS.cpp
@@ -1,9 +1,16 @@
 #include "pch.h"
-#include "CoreTLS.h"
+#include "SlabMemoryManager.h"
 
 thread_local uint32 tls_thread_id = 0;
 thread_local uint64 tls_end_tick_count = 0;
 thread_local stack<int32> tls_lock_stack;
 thread_local SharedSendBufferChunk tls_send_buffer_chunk;
 thread_local JobQueue* tls_current_job_queue = nullptr;
-thread_local ThreadLocalSlab tls_slab;
+thread_local ThreadLocalSlab** tls_slab = []() {
+	ThreadLocalSlab** arr = new ThreadLocalSlab*[kThreadSlabArrayCount];
+
+	for (int i = 0; i < kThreadSlabArrayCount; ++i)
+		arr[i] = new ThreadLocalSlab(SizeConverter::Instance().GetArray()[i]);
+
+	return arr;
+	}();
